f5229b3b2ded58e188f947af8ec290232b25bc3d
Aayush chouhan <aayushchouhanxyz27>
2025-12-14 20:09:41 +0530

    fix: correct backend entry path for render

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,10 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { connectDB } from "./lib/db.js";
-import { app, server } from "./lib/soket.js";
+import authRoutes from "./src/routes/auth.route.js";
+import messageRoutes from "./src/routes/message.route.js";
+import { connectDB } from "./src/lib/db.js";
+import { app, server } from "./src/lib/soket.js";
 
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
